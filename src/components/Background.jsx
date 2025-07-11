bc3c434afa3bad56aa6082e804eff82526f47a3d
Aniket Shinde <shindesam666@gmail.com>
2025-07-11 11:53:26 +0530

    fix: update header text

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -8,7 +8,7 @@ function Background() {
         My Notes
       </div>
       <h1 className='absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[13vw] leading-none tracking-tighter font-semibold text-zinc-900'>
-        Notes
+        InkSync
       </h1>
     </div>
   );
